6386e0ab9608b5a28817df9bd9a28d9032504a65
trigger <85785556+xhy330@users.noreply.github.com>
2023-02-13 11:42:18 +0800

    Add files via upload

--- BasicLevelClass/Class3/Code02_SmallSum.java
+++ BasicLevelClass/Class3/Code02_SmallSum.java
@@ -1,7 +1,22 @@
+/*
+* 在一个数组中，一个数左边比它小的数的总和，叫数的小和，所有数
+* 的小和累加起来，叫做数组小和，求数组小和
+* */
 package Class3;
 
 public class Code02_SmallSum {
 
+	/*
+	* 要求：求数组小和
+	* 思想：求一个数左边比它小的数的和，不就是说概数的右边有多少个比它大的数字，然后加几次就行。
+	* 	   采用归并排序的思路，每次递归都可以求出一部分的小和，并且归并排序的比较意义是会保留下来的
+	* 	   每次merge后，同组内的比较是不会再有的，大大降低了时间复杂度。
+	* 	   每个数的心路历程就是，我跟右组比较，只有有组存在比我大的数，那么我才会产生小和，每次merge都是如此，最后再把结果加起来
+	* 细节：跟归并排序的区别就是，merge所做的事不一样
+	*
+	* */
+
+	// 同等于归并排序的启动
 	public static int smallSum(int[] arr) {
 		if (arr == null || arr.length < 2) {
 			return 0;
@@ -9,16 +24,18 @@ public class Code02_SmallSum {
 		return process(arr, 0, arr.length - 1);
 	}
 
-	// arr[L..R]既要排好序，也要求小和返回
-	// 所有merge时，产生的小和，累加
-	// 左 排序   merge
-	// 右 排序  merge
-	// merge
+	/*
+	* arr[L..R]既要排好序，也要求小和返回
+	* 返回值是左边排序merge产生的所有小和+右边排序参数的小和，然后加上左右一起merge产生的小和。
+	* */
 	public static int process(int[] arr, int l, int r) {
 		if (l == r) {
 			return 0;
 		}
 		// l < r
+		// 找到中点，左侧排序然后merge求小和
+		// 右侧排序然后merge求小和
+		// 整个一起merge求小和
 		int mid = l + ((r - l) >> 1);
 		return 
 				process(arr, l, mid) 
@@ -28,6 +45,21 @@ public class Code02_SmallSum {
 				merge(arr, l, mid, r);
 	}
 
+	/*
+	* 关键的merge函数
+	* 思想：跟归并排序一样，整一个help数组来存排序后的数组，然后再拷贝回arr数组
+	* 参数：help：辅助数组
+	* 	   i：用来控制help数组的下标索引
+	* 	   p1：主要是来遍历arr数组的左半边，从L开始
+	* 	   p2：主要是来遍历arr数组的右半边，从m+1开始
+	*      res：记录小和，作为累加遍历
+	* 步骤：1. 在p1和p2不越界的情况下来遍历数组
+	*      2. 比较arr[p1]与arr[p2]，如果右边的数比左边大，那么就加上数组右边长度大小的当前左边数组指针所指的数字(arr[p1])
+	* 	   3. 否则就是不加，也就是+0
+	* 	   4. 右边比左边大，就加上小和，再就是，把左边的数给拷贝到help数组的当前位置，然后左边的索引和help数组索引往后移动一个，p1++，i++
+	* 	   5. 否则，如果右边的数，小于或等于左边数字，则直接拷贝右边数组，类似同上操作
+	* */
+
 	public static int merge(int[] arr, int L, int m, int r) {
 		int[] help = new int[r - L + 1];
 		int i = 0;
@@ -35,7 +67,10 @@ public class Code02_SmallSum {
 		int p2 = m + 1;
 		int res = 0;
 		while (p1 <= m && p2 <= r) {
+			// 求小和
+			// 利用右边有序的特性，将小和个数给榨出来
 			res += arr[p1] < arr[p2] ? (r - p2 + 1) * arr[p1] : 0;
+			// 左右组拷贝
 			help[i++] = arr[p1] < arr[p2] ? arr[p1++] : arr[p2++];
 		}
 		while (p1 <= m) {
